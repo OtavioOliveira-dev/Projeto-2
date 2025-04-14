7f1f374b9944dbd16036f5147faddbce2c24b472
OtavioOliveira <otavio.cristhian93@gmail.com>
2025-04-13 22:47:17 -0300

    Adicionado aumento de Salario

---
+++ AumentoSalario/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Globalization;
+using System.Xml;
+
+namespace AumentoSalario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Salario fun = new Salario();
+
+            Console.Write("Nome: ");
+            fun.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            fun.SalarioBruto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            fun.Imposto = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine( );
+
+            Console.WriteLine("Funcionário: "+ fun );
+            Console.WriteLine();
+
+            Console.Write("Digite a porcentagem do aumento:");
+
+            double pct = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+             fun.AlmentarSalario(pct);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: "+ fun);
+        }
+    }
+}
---
+++ AumentoSalario/Salario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AumentoSalario
+{
+    internal class Salario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AlmentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * (porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return Nome
+                +", $ "
+                +SalarioLiquido().ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
