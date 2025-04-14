3bd30eed7b649b6330209c747fdb2da2f9cdf3ad
OtavioOliveira <otavio.cristhian93@gmail.com>
2025-04-13 22:52:12 -0300

    Adicionado Circunferencia

---
+++ Cicunferencia/Calculadora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cicunferencia
+{
+    internal class Calculadora
+    {
+        public static double Pi = 3.14;
+
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r2)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r2, 3);
+        }
+
+    }
+}
---
+++ Cicunferencia/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization ;
+namespace Cicunferencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double vol = Calculadora.Volume(raio);
+
+            Console.WriteLine($"Circunferencia : {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume : {vol.ToString("F2",CultureInfo.InvariantCulture)} ");
+            Console.WriteLine($"Pi : {Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+       
+
+    }
+}
