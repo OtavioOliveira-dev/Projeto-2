f998cc9c3d01e565ab898437556d1413121d100f
OtavioOliveira <otavio.cristhian93@gmail.com>
2025-04-13 22:49:53 -0300

    Adicionado Cotação do Dollar

---
+++ CotacaoDollar/Cotação.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacaoDollar
+{
+    internal class Cotação
+    {
+        public static double IOF = 6.0;
+        public static double Convercao(double quantidade, double cotacao)
+        {
+            double total = quantidade * cotacao;
+            return total + total * IOF / 100.0;
+        }
+    }  
+}
---
+++ CotacaoDollar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CotacaoDollar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do Dollar: ");
+            double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Digite a quantidade de dollar que deseja comprar: ");
+            double quantidade = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = Cotação.Convercao(cotacao, quantidade );
+
+            Console.WriteLine($"Valor a ser pago em reais: {result.ToString("F2",CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
