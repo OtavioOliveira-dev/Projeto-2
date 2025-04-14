c4da326ddefb61fd790370f5b7122e345046e485
OtavioOliveira <otavio.cristhian93@gmail.com>
2025-04-13 22:53:47 -0300

    Adicionado Retangulo

---
+++ ProjetoRetangulo/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System;
+
+namespace ProjetoRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+            
+            Console.WriteLine("Entre com os valores: ");
+            r.Largura = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area = " + r.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + r.Perimetro().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + r.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
